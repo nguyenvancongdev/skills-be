0615eb542232ac8b8197e604df8b6ffff92fa9d9
nguyenvancongdev <congthang.nuce@gmail.com>
2022-05-15 22:17:18 +0700

    f

--- app/routes/User.route.js
+++ app/routes/User.route.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const route = express.Router();
+const createError = require('http-errors');
 //get a list of all User;
 route.get('/users', (req, res, next) => {
+    next(createError.InternalServerError())
+    // console.log(a)
     res.json({
         status: 'success',
         element: [{
--- index.js
+++ index.js
@@ -4,14 +4,29 @@ const helmet = require('helmet');
 const morgan = require('morgan')
 const PORT = process.env.PORT || 5000 ;
 const app = express();
-const userRoute = require('./app/routes/User.route')
+const createError = require('http-errors');
+const userRoute = require('./app/routes/User.route');
 app.use(helmet())
 app.use(morgan('common'))
+
 app.use('/v1/', userRoute)
 
 app.get('/',(req, res, next) => {
     res.send('this is Homes1')
 })
+app.use((req, res, next) => {
+    next(createError(404, 'Not Found !'))
+})
+app.use((err, req, res, next) => {
+    res.status(err.status|| 500)
+    res.json({
+        status: err.status || 500,
+        message: err.message,
+        toi: 'ttt'
+    })
+
+})
+
 app.listen(PORT, () => {
     console.log(`server-runer at ${PORT}`)
 })
